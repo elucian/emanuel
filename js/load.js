6c6f411126768bf2566571a62b769981f3a55ba7
Elucian Moise <elucian.moise@gmail.com>
2024-05-17 10:15:07 -0400

    fix

--- js/load.js
+++ js/load.js
@@ -1,52 +1,32 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const carouselInner = document.querySelector('.carousel-inner');
   
-    // Function to load images from the ../photo folder
     function loadImages() {
-      const photoFolder = '/photo';
-      const fileExtensions = ['.jpg', '.jpeg', '.png', '.gif']; // Add more extensions if needed
-  
-      fetch(photoFolder)
-        .then(response => {
-          console.log('Fetch response status:', response.status);
-          return response.text();
-        })
-        .then(data => {
-          const parser = new DOMParser();
-          const htmlDoc = parser.parseFromString(data, 'text/html');
-          const links = htmlDoc.querySelectorAll('a');
+      const baseUrl = '/photo/';
+      const startIndex = 1; // Adjust this value based on your naming convention
+      const endIndex = 36; // Adjust this value based on the number of images
 
-  
-          let activeSlide = true;
-  
-          links.forEach(link => {
-            const href = link.getAttribute('href');
-            const extension = href.slice(href.lastIndexOf('.'));
-            const imgSrc = href; // Update this line
+      let activeSlide = true;
 
-            if (fileExtensions.includes(extension)) {
-              const carouselItem = document.createElement('div');
-              carouselItem.classList.add('carousel-item');
-  
-              if (activeSlide) {
-                carouselItem.classList.add('active');
-                activeSlide = false;
-              }
-  
-              const img = document.createElement('img');
-              img.src = imgSrc;
-              img.classList.add('d-block', 'w-100');
-              img.loading = 'lazy'; // Enable lazy loading
-  
-              carouselItem.appendChild(img);
-              carouselInner.appendChild(carouselItem);
-            }
-          });
-        })
-        .catch(error => {
-          console.error('Error loading images:', error);
-        });
+      for (let i = startIndex; i <= endIndex; i++) {
+        const imageUrl = `${baseUrl}picture${i}.jpeg`;
+
+        const carouselItem = document.createElement('div');
+        carouselItem.classList.add('carousel-item');
+
+        if (activeSlide) {
+          carouselItem.classList.add('active');
+          activeSlide = false;
+        }
+
+        const img = document.createElement('img');
+        img.src = imageUrl;
+        img.classList.add('d-block', 'w-100');
+        img.loading = 'lazy';
+
+        carouselItem.appendChild(img);
+        carouselInner.appendChild(carouselItem);
+      }
     }
   
     loadImages();
